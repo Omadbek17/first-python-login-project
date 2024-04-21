90035ee78f78399b6e5c0db4c6a1687048568389
omadbek <qosimoff06@gmail.com>
2024-04-21 18:10:06 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#Calculator Project
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+
+print(c) # Qavs ichidagini o'zingiz Hohlagan amalingiizni javobini ko'rish uchun o'zgartirasiz
