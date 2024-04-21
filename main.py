d7a7b201b950a1ef177c0407fe7f0e2afcdf058d
omadbek <qosimoff06@gmail.com>
2024-04-21 18:16:55 +0500

    changed main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Calculator Project
 
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
+a = int(input("Enter first number: ")) #Bu yerda foydalanuvchi tomonidan son kiritishni so'rash kodi
+b = int(input("Enter second number: ")) #Bu yerda foydalanuvchi tomonidan ikkinchi son kiritishni so'rash kodi
 
 c = a + b
 d = a - b
@@ -9,3 +9,4 @@ e = a * b
 f = a / b
 
 print(c) # Qavs ichidagini o'zingiz Hohlagan amalingiizni javobini ko'rish uchun o'zgartirasiz
+
